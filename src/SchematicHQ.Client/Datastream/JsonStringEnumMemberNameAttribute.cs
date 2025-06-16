8b549d615ea3be994f58bea0b1c8c975757d5611
Christopher Brady <chris@schematichq.com>
2025-06-16 12:38:48 -0600

    change dotnet version used to build

---
+++ src/SchematicHQ.Client/Datastream/JsonStringEnumMemberNameAttribute.cs
@@ -0,0 +1,26 @@
+// using System;
+
+// namespace SchematicHQ.Client.Datastream
+// {
+//     /// <summary>
+//     /// Specifies the JSON serialization name for an enum field.
+//     /// </summary>
+//     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+//     public class JsonStringEnumMemberNameAttribute : Attribute
+//     {
+//         /// <summary>
+//         /// Gets the JSON serialization name for the enum field.
+//         /// </summary>
+//         public string Name { get; }
+
+//         /// <summary>
+//         /// Initializes a new instance of the <see cref="JsonStringEnumMemberNameAttribute"/> class
+//         /// with the specified name.
+//         /// </summary>
+//         /// <param name="name">The JSON serialization name for the enum field.</param>
+//         public JsonStringEnumMemberNameAttribute(string name)
+//         {
+//             Name = name;
+//         }
+//     }
+// }
