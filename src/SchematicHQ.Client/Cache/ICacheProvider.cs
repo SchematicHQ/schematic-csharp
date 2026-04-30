29ab4aeb0b1cc20330ce53106e2653a3654eccc4
JT <Hawxy@users.noreply.github.com>
2026-04-30 11:41:53 +0800

    Ensure value types are supported correctly.

--- src/SchematicHQ.Client/Cache/ICacheProvider.cs
+++ src/SchematicHQ.Client/Cache/ICacheProvider.cs
@@ -12,7 +12,7 @@ namespace SchematicHQ.Client.Cache
         /// <param name="key">Cache key</param>
         /// <param name="token">An optional cancellation token</param>
         /// <returns>The cached value or default if not found or expired</returns>
-        ValueTask<T?> Get<T>(string key, CancellationToken token = default);
+        ValueTask<T?> Get<T>(string key, CancellationToken token = default) where T: notnull;
 
         /// <summary>
         /// Set a value in the cache
@@ -21,7 +21,7 @@ namespace SchematicHQ.Client.Cache
         /// <param name="val">Value to cache</param>
         /// <param name="ttlOverride">Optional time-to-live override</param>
         /// <param name="token">An optional cancellation token</param>
-        ValueTask Set<T>(string key, T val, TimeSpan? ttlOverride = null, CancellationToken token = default);
+        ValueTask Set<T>(string key, T val, TimeSpan? ttlOverride = null, CancellationToken token = default) where T: notnull;
         
         /// <summary>
         /// Gets a value from the cache or if not present, uses the provided factory function.
@@ -30,7 +30,7 @@ namespace SchematicHQ.Client.Cache
         /// <param name="factory">The factory to generate the value</param>
         /// <param name="ttlOverride">Optional time-to-live override</param>
         /// <param name="token">An optional cancellation token</param>
-        ValueTask<T> GetOrSet<T>(string key, Func<CancellationToken, Task<T>> factory, TimeSpan? ttlOverride = null, CancellationToken token = default);
+        ValueTask<T> GetOrSet<T>(string key, Func<CancellationToken, Task<T>> factory, TimeSpan? ttlOverride = null, CancellationToken token = default) where T: notnull;
 
         /// <summary>
         /// Delete a key from the cache
